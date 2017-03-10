a388f5a3d92b42ed2c9507f693b99b38da82874c
MattMcMurray <mathieumcmurray@gmail.com>
2017-03-10 13:57:59 -0600

    Start building front-end "cards"

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
 
-gulp.task('nodemon', function (cb) {
+
+gulp.task('sass', function () {
+  return gulp.src('./public/css/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css/'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./public/css/*.scss', ['sass']);
+});
+
+gulp.task('nodemon', ['sass:watch'], function (cb) {
     var callbackCalled = false;
-    nodemon({script: './app.js'}).on('start', function () {
+    return nodemon({script: './app.js'}).on('start', function () {
         if (!callbackCalled) {
             callbackCalled = true;
             cb();
